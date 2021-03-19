92d3460269daeb85e3418d399f2e967c132726f4
niteesh99 <70364513+niteesh99@users.noreply.github.com>
2021-03-19 16:27:27 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,18 @@
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.github.com/users/VenkateshMogili', true)
+request.onload = function () 
+{
+  var data = JSON.parse(this.response);
+  document.getElementById("name").innerHTML=data.login;
+  document.getElementById("id1").innerHTML=data.id;
+  document.getElementById("node_id").innerHTML=data.node_id;
+  document.getElementById("avatar_url").innerHTML=data.avatar_url;
+  document.getElementById("Followers").innerHTML=data.avatar_url;
+  document.getElementById("created_at").innerHTML=data.avatar_url;
+  document.getElementById("site_admin").innerHTML=data.avatar_url;
+  
+  
+ 
+}
+request.send();
